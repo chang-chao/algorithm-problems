6d299415bc20aa600a132ebc53d56417b5b7cc19
chang chao <chang-chao@hotmail.com>
2017-04-03 12:58:21 +0900

    still  Time Limit Exceeded

--- src/chao/algorithm/leetcode/n30/SubstringWithConcatenationOfAllWords.java
+++ src/chao/algorithm/leetcode/n30/SubstringWithConcatenationOfAllWords.java
@@ -3,8 +3,10 @@ package chao.algorithm.leetcode.n30;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 public class SubstringWithConcatenationOfAllWords {
@@ -41,28 +43,24 @@ public class SubstringWithConcatenationOfAllWords {
       return false;
     }
 
-    Set<Integer> indices = new HashSet<>();
+    Map<String, List<Integer>> remainingWordMap = new HashMap<>();
+    Set<Entry<String, Set<Integer>>> entrySet = wordToIndexMap.entrySet();
+    for (Entry<String, Set<Integer>> entry : entrySet) {
+      remainingWordMap.put(entry.getKey(), new LinkedList<>(entry.getValue()));
+    }
+
     for (int i = 0; i < wordCount; i++) {
       String str = new String(sCharArray, index + wordSize * i, wordSize);
-      if (wordToIndexMap.containsKey(str)) {
-        Set<Integer> wordIndices = wordToIndexMap.get(str);
-
-        boolean added = false;
-        for (Integer wordIndex : wordIndices) {
-          if (!indices.contains(wordIndex)) {
-            indices.add(wordIndex);
-            added = true;
-            break;
-          }
-        }
-
-        if (!added) {
-          return false;
+      if (remainingWordMap.containsKey(str)) {
+        List<Integer> indexList = remainingWordMap.get(str);
+        indexList.remove(0);
+        if (indexList.isEmpty()) {
+          remainingWordMap.remove(str);
         }
       } else {
         return false;
       }
     }
-    return indices.size()==wordCount;
+    return remainingWordMap.isEmpty();
   }
 }
