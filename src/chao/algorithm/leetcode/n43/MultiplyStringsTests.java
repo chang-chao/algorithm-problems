d8d03e98f119fb8f14cf6d291dbfc76450d003e1
chang chao <chang-chao@hotmail.com>
2017-04-03 17:37:54 +0900

    time limit exceeded

--- src/chao/algorithm/leetcode/n43/MultiplyStringsTests.java
+++ src/chao/algorithm/leetcode/n43/MultiplyStringsTests.java
@@ -9,6 +9,7 @@ public class MultiplyStringsTests {
   @Test
   public void tests() {
     Assert.assertEquals("56088", sut.multiply("123", "456"));
+    Assert.assertEquals("2549938021076173291188348428453047981564820005480338416070", sut.multiply("8580858267709931299173190", "297165847695175076927326490812353"));
     Assert.assertEquals("0", sut.multiply("0", "0"));
   }
 }
