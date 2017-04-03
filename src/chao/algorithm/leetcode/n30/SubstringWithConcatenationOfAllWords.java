18129606ab971c9f34966aa5c7a5c701506be039
chang chao <chang-chao@hotmail.com>
2017-04-03 15:49:16 +0900

    try CharArrayRefString ,but failed

--- src/chao/algorithm/leetcode/n30/SubstringWithConcatenationOfAllWords.java
+++ src/chao/algorithm/leetcode/n30/SubstringWithConcatenationOfAllWords.java
@@ -35,6 +35,83 @@ public class SubstringWithConcatenationOfAllWords {
     return result;
   }
 
+  protected static class CharArrayRefString {
+    protected char[] charArray;
+    protected int offset;
+    protected int len;
+
+    CharArrayRefString(char[] charArray, int offset, int len) {
+      this.charArray = charArray;
+      this.offset = offset;
+      this.len = len;
+    }
+
+    public CharArrayRefString(String key) {
+      this.charArray = key.toCharArray();
+      this.offset = 0;
+      this.len = charArray.length;
+    }
+
+    public char[] getCharArray() {
+      return charArray;
+    }
+
+    public void setCharArray(char[] charArray) {
+      this.charArray = charArray;
+    }
+
+    public int getOffset() {
+      return offset;
+    }
+
+    public void setOffset(int offset) {
+      this.offset = offset;
+    }
+
+    public int getLen() {
+      return len;
+    }
+
+    public void setLen(int len) {
+      this.len = len;
+    }
+
+    @Override
+    public int hashCode() {
+      int result = 17;
+      for (int i = 0; i < len; i++) {
+        result += charArray[offset + i] * 31;
+      }
+      return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (obj == null) {
+        return false;
+      }
+      if (!(obj instanceof CharArrayRefString)) {
+        return false;
+      }
+
+      CharArrayRefString other = (CharArrayRefString) (obj);
+      if (len != other.getLen()) {
+        return false;
+
+      }
+
+      char[] otherCharArray = other.getCharArray();
+      for (int i = 0; i < len; i++) {
+        if (charArray[offset + i] != otherCharArray[other.getOffset() + i]) {
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+  }
+
   private boolean match(char[] sCharArray, int index, Map<String, Integer> wordToIndexMap, int wordSize,
       int wordCount) {
     int stringLen = sCharArray.length - index;
@@ -42,14 +119,14 @@ public class SubstringWithConcatenationOfAllWords {
       return false;
     }
 
-    Map<String, Integer> remainingWordMap = new HashMap<>();
+    Map<CharArrayRefString, Integer> remainingWordMap = new HashMap<>();
     Set<Entry<String, Integer>> entrySet = wordToIndexMap.entrySet();
     for (Entry<String, Integer> entry : entrySet) {
-      remainingWordMap.put(entry.getKey(), entry.getValue());
+      remainingWordMap.put(new CharArrayRefString(entry.getKey()), entry.getValue());
     }
 
     for (int i = 0; i < wordCount; i++) {
-      String str = new String(sCharArray, index + wordSize * i, wordSize);
+      CharArrayRefString str = new CharArrayRefString(sCharArray, index + wordSize * i, wordSize);
       if (remainingWordMap.containsKey(str)) {
         remainingWordMap.put(str, remainingWordMap.get(str) - 1);
         if (remainingWordMap.get(str) == 0) {
