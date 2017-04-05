0ddda74ac7813956a1d1b6a5fcf33ee95651040d
chang chao <chang-chao@hotmail.com>
2017-04-05 17:13:20 +0900

    ipo accept

--- src/chao/algorithm/leetcode/n502/Ipo.java
+++ src/chao/algorithm/leetcode/n502/Ipo.java
@@ -1,29 +1,30 @@
 package chao.algorithm.leetcode.n502;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.PriorityQueue;
 
 public class Ipo {
-  public int findMaximizedCapital(int k, int W, int[] Profits, int[] Capital) {
+  public int findMaximizedCapital(int k, int W, final int[] Profits, final int[] Capital) {
+    int size = Profits.length;
+    PriorityQueue<int[]> capQueue = new PriorityQueue<>(size, (o1, o2) -> Capital[o1[1]] - Capital[o2[1]]);
+    PriorityQueue<int[]> profitQueue = new PriorityQueue<>(size, (o1, o2) -> Profits[o2[1]] - Profits[o1[1]]);
+    for (int i = 0; i < size; i++) {
+      capQueue.add(new int[] { Capital[i], i });
+    }
     int currentW = W;
-    List<Integer> p = new ArrayList<>();
-    while (p.size() < k) {
-      int currentProject = -1;
-      int currentMaxProfit = 0;
-      for (int i = 0; i < Profits.length; i++) {
-        if (p.contains(i) || Capital[i] > currentW || Profits[i] < currentMaxProfit) {
-          continue;
-        }
-        currentProject = i;
-        currentMaxProfit = Profits[i];
+    int pSize = 0;
+    while (pSize < k) {
+      while (!capQueue.isEmpty() && capQueue.peek()[0] <= currentW) {
+        int[] project = capQueue.poll();
+        int index = project[1];
+        profitQueue.add(new int[] { Profits[index], index });
       }
 
-      if (currentProject == -1) {
-        break;
+      int[] nextProject = profitQueue.poll();
+      if (nextProject == null) {
+        return currentW;
       }
-
-      p.add(currentProject);
-      currentW += Profits[currentProject];
+      currentW += nextProject[0];
+      pSize++;
     }
     return currentW;
   }
