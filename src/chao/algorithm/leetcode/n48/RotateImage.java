6dbf8c31fded1f8113eac558db06dfd3b44327eb
chang chao <chang-chao@hotmail.com>
2018-04-17 17:03:36 +0900

    order

--- src/chao/algorithm/leetcode/n48/RotateImage.java
+++ src/chao/algorithm/leetcode/n48/RotateImage.java
@@ -2,13 +2,13 @@ package chao.algorithm.leetcode.n48;
 
 public class RotateImage {
   public void rotate(int[][] matrix) {
-    int indexN = matrix.length - 1;
     int temp;
+    int indexN = matrix.length - 1;
     for (int i = 0; i < matrix.length / 2; i++) {
       int ii = indexN - i;
       for (int j = i; j < ii; j++) {
-        temp = matrix[i][j];
         int jj = indexN - j;
+        temp = matrix[i][j];
         matrix[i][j] = matrix[jj][i];
         matrix[jj][i] = matrix[ii][jj];
         matrix[ii][jj] = matrix[j][ii];
