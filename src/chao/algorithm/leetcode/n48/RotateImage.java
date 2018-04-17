baeb62b40e99d1217ed218fe126c8b2e0d7c165d
chang chao <chang-chao@hotmail.com>
2018-04-17 11:15:23 +0900

    accepetd

--- src/chao/algorithm/leetcode/n48/RotateImage.java
+++ src/chao/algorithm/leetcode/n48/RotateImage.java
@@ -1,40 +1,33 @@
 package chao.algorithm.leetcode.n48;
 
-import java.util.Arrays;
-
 public class RotateImage {
   public void rotate(int[][] matrix) {
     int n = matrix.length;
     for (int circleIndex = 0; circleIndex < n / 2; circleIndex++) {
-      System.out.println("circleIndex=" + circleIndex);
       int nInThisCircle = n - 2 * circleIndex;
       int loopCountInCircle = nInThisCircle - 1;
 
       for (int loopIndexInCircle = 0; loopIndexInCircle < loopCountInCircle; loopIndexInCircle++) {
-        System.out.println("loopIndexInCircle=" + loopIndexInCircle);
-        int[] startPixel = { circleIndex, circleIndex + loopIndexInCircle };
 
-        int[] prevPixel = startPixel;
+        int[] prevPixel = { circleIndex, circleIndex + loopIndexInCircle };
+        int startPixelBackup = matrix[prevPixel[0]][prevPixel[1]];
         int[] nextPixel = { n - 1 - prevPixel[1], prevPixel[0] };
 
-        System.out.println(Arrays.toString(nextPixel) + " --> " + Arrays.toString(prevPixel));
         matrix[prevPixel[0]][prevPixel[1]] = matrix[nextPixel[0]][nextPixel[1]];
 
-        prevPixel = nextPixel;
+        prevPixel[0] = nextPixel[0];
+        prevPixel[1] = nextPixel[1];
         nextPixel[0] = n - 1 - prevPixel[1];
         nextPixel[1] = prevPixel[0];
-        System.out.println(Arrays.toString(nextPixel) + " --> " + Arrays.toString(prevPixel));
         matrix[prevPixel[0]][prevPixel[1]] = matrix[nextPixel[0]][nextPixel[1]];
 
-        prevPixel = nextPixel;
+        prevPixel[0] = nextPixel[0];
+        prevPixel[1] = nextPixel[1];
         nextPixel[0] = n - 1 - prevPixel[1];
         nextPixel[1] = prevPixel[0];
-        System.out.println(Arrays.toString(nextPixel) + " --> " + Arrays.toString(prevPixel));
         matrix[prevPixel[0]][prevPixel[1]] = matrix[nextPixel[0]][nextPixel[1]];
 
-        prevPixel = nextPixel;
-        System.out.println(Arrays.toString(nextPixel) + " --> " + Arrays.toString(prevPixel));
-        matrix[prevPixel[0]][prevPixel[1]] = matrix[startPixel[0]][startPixel[1]];
+        matrix[nextPixel[0]][nextPixel[1]] = startPixelBackup;
       }
 
     }
--- src/chao/algorithm/leetcode/n48/RotateImageTests.java
+++ src/chao/algorithm/leetcode/n48/RotateImageTests.java
@@ -1,6 +1,5 @@
 package chao.algorithm.leetcode.n48;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 public class RotateImageTests {
@@ -8,7 +7,10 @@ public class RotateImageTests {
 
   @Test
   public void tests() {
-    sut.rotate(new int[][] { { 2, 29, 20, 26, 16, 28 }, { 12, 27, 9, 25, 13, 21 }, { 32, 33, 32, 2, 28, 14 },
-        { 13, 14, 32, 27, 22, 26 }, { 33, 1, 20, 7, 21, 7 }, { 4, 24, 1, 6, 32, 34 } });
+//    sut.rotate(new int[][] { { 2, 29, 20, 26, 16, 28 }, { 12, 27, 9, 25, 13, 21 }, { 32, 33, 32, 2, 28, 14 },
+//        { 13, 14, 32, 27, 22, 26 }, { 33, 1, 20, 7, 21, 7 }, { 4, 24, 1, 6, 32, 34 } });
+
+    sut.rotate(new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } });
+
   }
 }
