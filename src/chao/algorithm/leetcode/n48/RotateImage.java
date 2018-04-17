612e1a4350ab904ce3d84808f2d6e48c1afe57cd
chang chao <chang-chao@hotmail.com>
2018-04-17 16:44:56 +0900

    beat 100%

--- src/chao/algorithm/leetcode/n48/RotateImage.java
+++ src/chao/algorithm/leetcode/n48/RotateImage.java
@@ -2,21 +2,17 @@ package chao.algorithm.leetcode.n48;
 
 public class RotateImage {
   public void rotate(int[][] matrix) {
-    for (int circleIndex = 0; circleIndex < matrix.length / 2; circleIndex++) {
-      copySingleCircle(matrix, circleIndex);
-    }
-  }
-
-  private void copySingleCircle(int[][] matrix, int circleIndex) {
     int indexN = matrix.length - 1;
-    for (int loopIndexInCircle = 0; loopIndexInCircle < indexN - 2 * circleIndex; loopIndexInCircle++) {
-      int i = circleIndex;
-      int j = circleIndex + loopIndexInCircle;
-      int ijCopy = matrix[i][j];
-      matrix[i][j] = matrix[indexN - j][i];
-      matrix[indexN - j][i] = matrix[indexN - i][indexN - j];
-      matrix[indexN - i][indexN - j] = matrix[j][indexN - i];
-      matrix[j][indexN - i] = ijCopy;
+    for (int circleIndex = 0; circleIndex < matrix.length / 2; circleIndex++) {
+      for (int loopIndexInCircle = 0; loopIndexInCircle < indexN - 2 * circleIndex; loopIndexInCircle++) {
+        int i = circleIndex;
+        int j = circleIndex + loopIndexInCircle;
+        int ijCopy = matrix[i][j];
+        matrix[i][j] = matrix[indexN - j][i];
+        matrix[indexN - j][i] = matrix[indexN - i][indexN - j];
+        matrix[indexN - i][indexN - j] = matrix[j][indexN - i];
+        matrix[j][indexN - i] = ijCopy;
+      }
     }
   }
 }
