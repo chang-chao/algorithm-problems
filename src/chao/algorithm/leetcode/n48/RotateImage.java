65b774facd77a5cf9879d9e577d8b49198ff50e4
chang chao <chang-chao@hotmail.com>
2018-04-17 16:42:29 +0900

    no copy

--- src/chao/algorithm/leetcode/n48/RotateImage.java
+++ src/chao/algorithm/leetcode/n48/RotateImage.java
@@ -1,31 +1,22 @@
 package chao.algorithm.leetcode.n48;
 
-import java.util.stream.IntStream;
-
 public class RotateImage {
   public void rotate(int[][] matrix) {
-    IntStream.range(0, matrix.length / 2).forEach(circleIndex -> copySingleCircle(matrix, circleIndex));
+    for (int circleIndex = 0; circleIndex < matrix.length / 2; circleIndex++) {
+      copySingleCircle(matrix, circleIndex);
+    }
   }
 
   private void copySingleCircle(int[][] matrix, int circleIndex) {
-    int n = matrix.length;
-    for (int loopIndexInCircle = 0; loopIndexInCircle < n - 2 * circleIndex - 1; loopIndexInCircle++) {
-
-      int[] pos1 = { circleIndex, circleIndex + loopIndexInCircle };
-      int[] pos2 = { n - 1 - pos1[1], pos1[0] };
-
-      int startPixelBackup = matrix[pos1[0]][pos1[1]];
-      matrix[pos1[0]][pos1[1]] = matrix[pos2[0]][pos2[1]];
-
-      pos1[0] = n - 1 - pos2[1];
-      pos1[1] = pos2[0];
-      matrix[pos2[0]][pos2[1]] = matrix[pos1[0]][pos1[1]];
-
-      pos2[0] = n - 1 - pos1[1];
-      pos2[1] = pos1[0];
-      matrix[pos1[0]][pos1[1]] = matrix[pos2[0]][pos2[1]];
-
-      matrix[pos2[0]][pos2[1]] = startPixelBackup;
+    int indexN = matrix.length - 1;
+    for (int loopIndexInCircle = 0; loopIndexInCircle < indexN - 2 * circleIndex; loopIndexInCircle++) {
+      int i = circleIndex;
+      int j = circleIndex + loopIndexInCircle;
+      int ijCopy = matrix[i][j];
+      matrix[i][j] = matrix[indexN - j][i];
+      matrix[indexN - j][i] = matrix[indexN - i][indexN - j];
+      matrix[indexN - i][indexN - j] = matrix[j][indexN - i];
+      matrix[j][indexN - i] = ijCopy;
     }
   }
 }
