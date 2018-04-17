7e0779f08df1fa2614a9d8766621df77a71d3786
chang chao <chang-chao@hotmail.com>
2018-04-17 11:30:45 +0900

    final

--- src/chao/algorithm/leetcode/n48/RotateImage.java
+++ src/chao/algorithm/leetcode/n48/RotateImage.java
@@ -4,10 +4,8 @@ public class RotateImage {
   public void rotate(int[][] matrix) {
     int n = matrix.length;
     for (int circleIndex = 0; circleIndex < n / 2; circleIndex++) {
-      int nInThisCircle = n - 2 * circleIndex;
-      int loopCountInCircle = nInThisCircle - 1;
 
-      for (int loopIndexInCircle = 0; loopIndexInCircle < loopCountInCircle; loopIndexInCircle++) {
+      for (int loopIndexInCircle = 0; loopIndexInCircle < n - 2 * circleIndex - 1; loopIndexInCircle++) {
 
         int[] pos1 = { circleIndex, circleIndex + loopIndexInCircle };
         int[] pos2 = { n - 1 - pos1[1], pos1[0] };
@@ -15,7 +13,6 @@ public class RotateImage {
         int startPixelBackup = matrix[pos1[0]][pos1[1]];
         matrix[pos1[0]][pos1[1]] = matrix[pos2[0]][pos2[1]];
 
-     
         pos1[0] = n - 1 - pos2[1];
         pos1[1] = pos2[0];
         matrix[pos2[0]][pos2[1]] = matrix[pos1[0]][pos1[1]];
