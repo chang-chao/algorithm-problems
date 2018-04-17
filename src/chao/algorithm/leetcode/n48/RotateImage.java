b30d617a414fa85bd090ed25684708a58c7b2c80
chang chao <chang-chao@hotmail.com>
2018-04-17 17:07:15 +0900

    Declaring objects in the smallest scope

--- src/chao/algorithm/leetcode/n48/RotateImage.java
+++ src/chao/algorithm/leetcode/n48/RotateImage.java
@@ -2,13 +2,12 @@ package chao.algorithm.leetcode.n48;
 
 public class RotateImage {
   public void rotate(int[][] matrix) {
-    int temp;
     int indexN = matrix.length - 1;
     for (int i = 0; i < matrix.length / 2; i++) {
       int ii = indexN - i;
       for (int j = i; j < ii; j++) {
         int jj = indexN - j;
-        temp = matrix[i][j];
+        int temp = matrix[i][j];
         matrix[i][j] = matrix[jj][i];
         matrix[jj][i] = matrix[ii][jj];
         matrix[ii][jj] = matrix[j][ii];
